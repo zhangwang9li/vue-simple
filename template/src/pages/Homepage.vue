610f519e4ad91ce69c011bcbd0392df1db2d66d6
张浩 <zhangwang9li@163.com>
2018-02-26 13:39:03 +0800

    English description document

---
+++ template/src/pages/Homepage.vue
@@ -0,0 +1,22 @@
+/**
+* Created by zhanghao on 2018/2/26.
+*/
+<template>
+</template>
+
+<script>
+    export default {
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {},
+        mounted() {
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
