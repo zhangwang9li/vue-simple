bf1a2347a9382330d03038f360978dcee3c3a606
张浩 <zhangwang9li@163.com>
2018-03-05 11:17:43 +0800

    丰富系统主页mainpage

--- template/src/pages/MainPage.vue
+++ template/src/pages/MainPage.vue
@@ -2,18 +2,31 @@
 * Created by zhanghao on 2018/2/26.
 */
 <template>
+  <VerticalLayout hHeightOpen="84px">
+    <div slot="header" class="layout-div main-header" style="">
+      请根据需求编写内容
+    </div>
+    <div class="layout-div">
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
+    </div>
+  </VerticalLayout>
 </template>
 
 <script>
-    export default {
-        components: {},
-        data() {
-            return {}
-        },
-        methods: {},
-        mounted() {
-        }
+  import HorizontalLayout from '../components/HorizontalLayout.vue'
+  import VerticalLayout from '../components/VerticalLayout.vue'
+
+  export default {
+    components: {HorizontalLayout, VerticalLayout},
+    data() {
+      return {}
+    },
+    methods: {},
+    mounted() {
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
